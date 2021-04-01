6d3873177923a6b5ad311795853908b1be738d91
Antoine Girard <antoine.girard@ing.com>
2021-04-01 15:33:43 +0200

    Handle template files

--- tests/dos_lib_tests.py
+++ tests/dos_lib_tests.py
@@ -13,6 +13,7 @@ class DosLibTests(unittest.TestCase):
     def test_rand_str(self):
         str1 = dos_lib("rand_str")
         str2 = dos_lib("rand_str")
-        self.assertEqual(33, len(str1))
-        self.assertEqual(16, len(bytes.fromhex(str1)))
+
+        self.assertEqual(17, len(str1))
+        self.assertEqual(8, len(bytes.fromhex(str1)))
         self.assertNotEqual(str1, str2)
